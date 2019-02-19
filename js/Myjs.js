2ff694f7349c480e0528bcdabe88e99dc97c6ab0
jmelton22 <jmelton22@gmail.com>
2019-02-19 10:38:30 -0500

    Initial Commit

---
+++ js/Myjs.js
@@ -0,0 +1,142 @@
+//Init Map
+//*******************************************************************************************************************************************************
+var lat = 41.141376;
+var lng = -8.613999;
+var zoom = 14;
+
+// add an OpenStreetMap tile layer
+var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiemdYSVVLRSJ9.g3lbg_eN0kztmsfIPxa9MQ';
+
+
+var grayscale = L.tileLayer(mbUrl, {
+        id: 'mapbox.light',
+        attribution: mbAttr
+    }),
+    streets = L.tileLayer(mbUrl, {
+        id: 'mapbox.streets',
+        attribution: mbAttr
+    });
+
+
+var map = L.map('map', {
+    center: [lat, lng], // Porto
+    zoom: zoom,
+    layers: [streets],
+    zoomControl: true,
+    fullscreenControl: true,
+    fullscreenControlOptions: { // optional
+        title: "Show me the fullscreen!",
+        titleCancel: "Exit fullscreen mode",
+        position: 'bottomright'
+    }
+});
+
+var baseLayers = {
+    "Grayscale": grayscale, // Grayscale tile layer
+    "Streets": streets, // Streets tile layer
+};
+
+layerControl = L.control.layers(baseLayers, null, {
+    position: 'bottomleft'
+}).addTo(map);
+
+// Initialise the FeatureGroup to store editable layers
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+
+var featureGroup = L.featureGroup();
+
+var drawControl = new L.Control.Draw({
+    position: 'bottomright',
+	collapsed: false,
+    draw: {
+        // Available Shapes in Draw box. To disable anyone of them just convert true to false
+        polyline: false,
+        polygon: false,
+        circle: false,
+        rectangle: true,
+        marker: false,
+    }
+});
+map.addControl(drawControl); // To add anything to map, add it to "drawControl"
+//*******************************************************************************************************************************************************
+//*****************************************************************************************************************************************
+// Index Road Network by Using R-Tree
+//*****************************************************************************************************************************************
+var rt = cw(function(data, cb) {
+	var self = this;
+	var request, _resp;
+	importScripts("js/rtree.js");
+	if(!self.rt) {
+		self.rt = RTree();
+		request = new XMLHttpRequest();
+		request.open("GET", data);
+		request.onreadystatechange = function() {
+			if (request.readyState === 4 && request.status === 200) {
+				_resp = JSON.parse(request.responseText);
+				self.rt.geoJSON(_resp);
+				cb(true);
+			}
+		};
+		request.send();
+	}else{
+		return self.rt.bbox(data);
+	}
+});
+
+rt.data(cw.makeUrl("js/trips.json"));
+//*****************************************************************************************************************************************	
+//*****************************************************************************************************************************************
+// Drawing Shapes (polyline, polygon, circle, rectangle, marker) Event:
+// Select from draw box and start drawing on map.
+//*****************************************************************************************************************************************	
+
+map.on('draw:created', function(e) {
+	
+	var type = e.layerType,
+		layer = e.layer;
+	
+	if (type === 'rectangle') {
+		console.log(layer.getLatLngs()); //Rectangle Corners points
+		var bounds = layer.getBounds();
+		rt.data([[bounds.getSouthWest().lng, bounds.getSouthWest().lat],
+				[bounds.getNorthEast().lng, bounds.getNorthEast().lat]])
+			.then(function(d){var result = d.map(function(a) { return a.properties; });
+		console.log(result);		// Trip Info: avspeed, distance, duration, endtime, maxspeed, minspeed, starttime, streetnames, taxiid, tripid
+		DrawRS(result);
+
+		// TODO: Add D3 visualization functions, ex: drawGraph(result)
+
+		});
+	}
+	
+	drawnItems.addLayer(layer);			//Add your Selection to Map
+
+	// TODO: Add layers to map (heatmap, start/end points)
+
+});
+//*****************************************************************************************************************************************
+// DrawRS Function:
+// Input is a list of road segments ID and their color. Then the visualization can show the corresponding road segments with the color
+// Test:      var input_data = [{road:53, color:"#f00"}, {road:248, color:"#0f0"}, {road:1281, color:"#00f"}];
+//            DrawRS(input_data);
+//*****************************************************************************************************************************************
+function DrawRS(trips) {
+	for (var j = 0; j < trips.length; j++) {  // Check Number of Segments and go through all segments
+		var TPT = [];
+		TPT = TArr[trips[j].tripid].split(',');  		 // Find each segment in TArr Dictionary. 
+		var polyline = new L.Polyline([]).addTo(drawnItems);
+        polyline.setStyle({
+            color: 'red',                      // polyline color
+			weight: 1,                         // polyline weight
+			opacity: 0.5,                      // polyline opacity
+			smoothFactor: 1.0  
+        });
+		for(var y = 0; y < TPT.length - 1; y = y+2){    // Parse latlng for each segment
+			polyline.addLatLng([parseFloat(TPT[y+1]), parseFloat(TPT[y])]);
+		}
+	}		
+}
